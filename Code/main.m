bfc101ee0eccf52ee7892e7ee3010448902b8ae2
eanhall <eanhall@berkeley.edu>
2019-12-12 13:42:56 -0800

    Moved commented section to the bottom.

--- Code/main.m
+++ Code/main.m
@@ -7,45 +7,7 @@ clc
 % Load Dynamics
 Dynamics;
 
-%% Continuous System Test
-% The quadcopter should slow down after each input and hover at the end.
-% u = [0.25*mass*g*ones(4,1);g];
-% u1 = [0.25*1.2*mass*g*ones(4,1);g];
-% u2 = [0.25*0.8*mass*g*ones(4,1);g];
-% u = repmat(u,1,51);
-% u1 = repmat(u1,1,75);
-% u2 = repmat(u2,1,75);
-% u = [u1 u2 u];
-% t = 0:0.1:20;
-% x0 = [2;0;0;0;1;0;0;0;1;0;0;0];
-% [y,t,x] = lsim(system,u,t,x0);
 
-%% Discrete System Test
-% The quadcopter should slow down after each input and hover at the end.
-% u1 = [0.25*1.4*mass*g*ones(4,1);g];
-% u2 = [0.25*0.6*mass*g*ones(4,1);g];
-% u4 = [0.25*mass*g*ones(4,1);g];
-% 
-% N = 25;
-% u1 = repmat(u1,1,10);
-% u2 = repmat(u2,1,10);
-% u4 = repmat(u4,1,5);
-% u = [u1 u2 u4];
-% x0 = zeros(12,1);
-% x = zeros(12,N+1);
-% x(:,1) = x0;
-% 
-% for i = 1:N
-%     x(:,i+1) = A*x(:,i) + B*u(:,i);
-% end
-% 
-% figure(1)
-% plot3(x(1,:),x(5,:),x(9,:))
-% xlim([0 1])
-% ylim([0 1])
-% 
-% figure(2)
-% plot(x(9,:))
 
 %% MPC Horizon
 N = 25;
@@ -187,9 +149,49 @@ legend('Motor 1','Motor 2','Motor 3','Motor 4')
 %     predErr(:,i) = [norm(err(1,:)) norm(err(2,:))]';
 % end
 
-xTest = zeros(n,M+1);
-xTest(:,1) = x0;
+% xTest = zeros(n,M+1);
+% xTest(:,1) = x0;
+% 
+% for i = 1:M
+%    xTest(:,i+1) = A*xTest(:,i) + B*uOpt(:,i); 
+% end
 
-for i = 1:M
-   xTest(:,i+1) = A*xTest(:,i) + B*uOpt(:,i); 
-end
+%% Continuous System Test
+% The quadcopter should slow down after each input and hover at the end.
+% u = [0.25*mass*g*ones(4,1);g];
+% u1 = [0.25*1.2*mass*g*ones(4,1);g];
+% u2 = [0.25*0.8*mass*g*ones(4,1);g];
+% u = repmat(u,1,51);
+% u1 = repmat(u1,1,75);
+% u2 = repmat(u2,1,75);
+% u = [u1 u2 u];
+% t = 0:0.1:20;
+% x0 = [2;0;0;0;1;0;0;0;1;0;0;0];
+% [y,t,x] = lsim(system,u,t,x0);
+
+%% Discrete System Test
+% The quadcopter should slow down after each input and hover at the end.
+% u1 = [0.25*1.4*mass*g*ones(4,1);g];
+% u2 = [0.25*0.6*mass*g*ones(4,1);g];
+% u4 = [0.25*mass*g*ones(4,1);g];
+% 
+% N = 25;
+% u1 = repmat(u1,1,10);
+% u2 = repmat(u2,1,10);
+% u4 = repmat(u4,1,5);
+% u = [u1 u2 u4];
+% x0 = zeros(12,1);
+% x = zeros(12,N+1);
+% x(:,1) = x0;
+% 
+% for i = 1:N
+%     x(:,i+1) = A*x(:,i) + B*u(:,i);
+% end
+% 
+% figure(1)
+% plot3(x(1,:),x(5,:),x(9,:))
+% xlim([0 1])
+% ylim([0 1])
+% 
+% figure(2)
+% plot(x(9,:))
