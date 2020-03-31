d08ff5661b19a39a46f10b0972ab3ce3fefe2383
eanhall <eanhall@berkeley.edu>
2020-03-31 12:53:25 -0700

    Changed to a rates controller.

--- Code/Dynamics.m
+++ Code/Dynamics.m
@@ -2,13 +2,13 @@
 clear
 clc
 % Variables
-syms s1 v1 theta q s2 v2 phi p s3 v3 psi r T n1 n2 n3 cp1 cp2 cp3 cp4
+syms s1 v1 theta s2 v2 phi s3 v3 psi T p q r g
 pos = [s1;s2;s3];
 vel = [v1;v2;v3];
 orien = [phi;theta;psi];
-rates = [p;q;r];
+u = [T;p;q;r];
 % u = [T;n1;n2;n3];
-u = [cp1;cp2;cp3;cp4];
+% u = [cp1;cp2;cp3;cp4];
 mass = 0.688;
 l = 0.12;
 k = 0.014;
@@ -16,18 +16,15 @@ J = (10^-4)*[36 36 74];
 g = 9.81;
 
 %% State Space
-A = [0 1 0 0 0 0 0 0 0 0 0 0
-     0 0 g 0 0 0 0 0 0 0 0 0
-     0 0 0 1 0 0 0 0 0 0 0 0
-     0 0 0 0 0 0 0 0 0 0 0 0
-     0 0 0 0 0 1 0 0 0 0 0 0
-     0 0 0 0 0 0 -g 0 0 0 0 0
-     0 0 0 0 0 0 0 1 0 0 0 0
-     0 0 0 0 0 0 0 0 0 0 0 0
-     0 0 0 0 0 0 0 0 0 1 0 0
-     0 0 0 0 0 0 0 0 0 0 0 0
-     0 0 0 0 0 0 0 0 0 0 0 1
-     0 0 0 0 0 0 0 0 0 0 0 0];
+A = [0 1 0 0 0 0 0 0 0
+     0 0 g 0 0 0 0 0 0
+     0 0 0 0 0 0 0 0 0
+     0 0 0 0 1 0 0 0 0
+     0 0 0 0 0 -g 0 0 0
+     0 0 0 0 0 0 0 0 0
+     0 0 0 0 0 0 0 1 0
+     0 0 0 0 0 0 0 0 0
+     0 0 0 0 0 0 0 0 0];
  
 %  B = [0 0 0 0
 %       0 0 0 0
@@ -42,18 +39,28 @@ A = [0 1 0 0 0 0 0 0 0 0 0 0
 %       0 0 1/J(2) 0
 %       0 0 0 1/J(3)];
 
+%  B = [0 0 0 0 0
+%       0 0 0 0 0
+%       0 0 0 0 0 
+%       -l/J(2) -l/J(2) l/J(2) l/J(2) 0
+%       0 0 0 0 0
+%       0 0 0 0 0
+%       0 0 0 0 0
+%       l/J(1) -l/J(1) -l/J(1) l/J(1) 0
+%       0 0 0 0 0
+%       1/mass 1/mass 1/mass 1/mass -1
+%       0 0 0 0 0
+%       k/J(3) -k/J(3) k/J(3) -k/J(3) 0];
+
  B = [0 0 0 0 0
       0 0 0 0 0
-      0 0 0 0 0 
-      -l/J(2) -l/J(2) l/J(2) l/J(2) 0
+      0 0 1 0 0 
       0 0 0 0 0
       0 0 0 0 0
+      0 1 0 0 0
       0 0 0 0 0
-      l/J(1) -l/J(1) -l/J(1) l/J(1) 0
-      0 0 0 0 0
-      1/mass 1/mass 1/mass 1/mass -1
-      0 0 0 0 0
-      k/J(3) -k/J(3) k/J(3) -k/J(3) 0];
+      1/mass 0 0 0 -1
+      0 0 0 1 0];
   
 C = eye(12);
 
@@ -76,7 +83,7 @@ B = dsys.B;
 C = dsys.C;
 D = dsys.D;
 
-uRef = mass*g/4*ones(4,1); % For other kind of final states run optimization from slides
+uRef = [mass*g;0;0;0]; % For other kind of final states run optimization from slides
 
 %% Model mismatch:
 
