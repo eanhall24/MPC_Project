b041be1721acdd70ab4f86600311d0bfda6813b7
eanhall <eanhall@berkeley.edu>
2020-04-25 23:45:08 -0700

    Adding braking analysis

---
+++ Analysis/Braking_Analysis.m
@@ -0,0 +1,4 @@
+%% Braking Analysis
+% The following data was obtained by calculating the optimal inputs for the
+% control objective, and publishing all the inputs through a feedforward
+% method.
--- Analysis/Gain_Analysis.m
+++ Analysis/Gain_Analysis.m
@@ -116,8 +116,6 @@ rates_settlingTime = settlingTime;
 telTindex = floor(Tindex/2);
 teltime = teltime - teltime(telTindex);
 
-ratesMax = max(mot1);
-
 figure('Name','Rates')
 subplot(2,1,1)
 plot(time(Tindex:end),posx(Tindex:end),'b')
@@ -144,16 +142,21 @@ ylabel('Motor Force(N)')
 ylim([1.5 2.5])
 legend('motor 1','motor 2','motor 3','motor 4')
 
-disp('The settling time for the MPC controller is:')
+disp('The settling time for the rates controller is:')
 disp(settlingTime)
 
-%% MPC @ [1,2,2]
+%% Rates Forward @ [2,0,1]
 clc
 
-load('MPC1_posx.mat')
-load('MPC1_posy.mat')
-load('MPC1_posz.mat')
-load('MPC1_time.mat')
+load('Forward_Rates_posx.mat')
+load('Forward_Rates_posy.mat')
+load('Forward_Rates_posz.mat')
+load('Forward_Rates_time.mat')
+load('Forward_Rates_teltime.mat')
+load('Forward_Rates_mot1.mat')
+load('Forward_Rates_mot2.mat')
+load('Forward_Rates_mot3.mat')
+load('Forward_Rates_mot4.mat')
 
 Tindex = 1;
 
@@ -165,22 +168,37 @@ for i = 1:length(posz)
 end
 
 time = time - time(Tindex);
-desPos = [1;2;2];
+
+for i = 1:length(time)
+    if time(i) > 6
+        Tindex = i;
+        break;
+    end
+end
+
+desPos = [2;0;0];
 lower = desPos - 0.02*desPos;
 upper = desPos + 0.02*desPos;
 ss_index = 1;
 
-for i = Tindex:length(posz)
-    if (posx(i) < lower(1) || posx(i) > upper(1)) ||...
-            (posy(i) < lower(2) || posy(i) > upper(2)) ||...
-            (posz(i) < lower(3) || posz(i) > upper(3))
+for i = Tindex:length(posx)
+    if posx(i) < lower(1) || posx(i) > upper(1)
         ss_index = i;
     end
 end
 
 settlingTime = time(ss_index);
+forward_rates_settlingTime = settlingTime;
 
-figure('Name','MPC_')
+telTindex = floor(Tindex/2);
+teltime = teltime - teltime(telTindex);
+
+[M,I] = max(mot3);
+
+ratesinput = [mot1(I); mot2(I); mot3(I); mot4(I)];
+
+figure('Name','Forward Rates')
+subplot(2,1,1)
 plot(time(Tindex:end),posx(Tindex:end),'b')
 hold on
 plot(time(Tindex:end),posy(Tindex:end),'g')
@@ -188,24 +206,38 @@ plot(time(Tindex:end),posz(Tindex:end),'r')
 xline(settlingTime,'k');
 yline(lower(1),'--k');
 yline(upper(1),'--k');
-yline(lower(3),'--k');
-yline(upper(3),'--k');
 ylim([0 2.1])
 xlabel('Time(s)')
 ylabel('Position(m)')
 legend('posx','posy','posz','settling time')
-title('MPC: Target [1,2,2]')
+title('Rates: Target [2,0,1]')
 
-disp('The settling time for the MPC controller is:')
+subplot(2,1,2)
+plot(teltime(telTindex:end),mot1(telTindex:end))
+hold on
+plot(teltime(telTindex:end),mot2(telTindex:end))
+plot(teltime(telTindex:end),mot3(telTindex:end))
+plot(teltime(telTindex:end),mot4(telTindex:end))
+xlabel('Time(s)')
+ylabel('Motor Force(N)')
+% ylim([1.5 2.5])
+legend('motor 1','motor 2','motor 3','motor 4')
+
+disp('The settling time for the rates controller is:')
 disp(settlingTime)
 
-%% Rates @ [1,2,2]
+%% Unconstrained MPC Forward @ [2,0,1]
 clc
 
-load('Rates1_posx.mat')
-load('Rates1_posy.mat')
-load('Rates1_posz.mat')
-load('Rates1_time.mat')
+load('Forward_UMPC_posx.mat')
+load('Forward_UMPC_posy.mat')
+load('Forward_UMPC_posz.mat')
+load('Forward_UMPC_time.mat')
+load('Forward_UMPC_teltime.mat')
+load('Forward_UMPC_mot1.mat')
+load('Forward_UMPC_mot2.mat')
+load('Forward_UMPC_mot3.mat')
+load('Forward_UMPC_mot4.mat')
 
 Tindex = 1;
 
@@ -217,22 +249,37 @@ for i = 1:length(posz)
 end
 
 time = time - time(Tindex);
-desPos = [1;2;2];
+
+for i = 1:length(time)
+    if time(i) > 6
+        Tindex = i;
+        break;
+    end
+end
+
+desPos = [2;0;0];
 lower = desPos - 0.02*desPos;
 upper = desPos + 0.02*desPos;
 ss_index = 1;
 
-for i = Tindex:length(posz)
-    if (posx(i) < lower(1) || posx(i) > upper(1)) ||...
-            (posy(i) < lower(2) || posy(i) > upper(2)) ||...
-            (posz(i) < lower(3) || posz(i) > upper(3))
+for i = Tindex:length(posx)
+    if posx(i) < lower(1) || posx(i) > upper(1)
         ss_index = i;
     end
 end
 
 settlingTime = time(ss_index);
+forward_umpc_settlingTime = settlingTime;
 
-figure('Name','Rates_')
+telTindex = floor(Tindex/2);
+teltime = teltime - teltime(telTindex);
+
+[M,I] = max(mot3);
+
+umpcinput = [mot1(I); mot2(I); mot3(I); mot4(I)];
+
+figure('Name','Forward Unconstrained MPC')
+subplot(2,1,1)
 plot(time(Tindex:end),posx(Tindex:end),'b')
 hold on
 plot(time(Tindex:end),posy(Tindex:end),'g')
@@ -240,14 +287,107 @@ plot(time(Tindex:end),posz(Tindex:end),'r')
 xline(settlingTime,'k');
 yline(lower(1),'--k');
 yline(upper(1),'--k');
-yline(lower(3),'--k');
-yline(upper(3),'--k');
 ylim([0 2.1])
 xlabel('Time(s)')
 ylabel('Position(m)')
 legend('posx','posy','posz','settling time')
-title('Rates: Target [1,2,2]')
+title('MPC: Target [2,0,1]')
 
-disp('The settling time for the MPC controller is:')
+subplot(2,1,2)
+plot(teltime(telTindex:end),mot1(telTindex:end))
+hold on
+plot(teltime(telTindex:end),mot2(telTindex:end))
+plot(teltime(telTindex:end),mot3(telTindex:end))
+plot(teltime(telTindex:end),mot4(telTindex:end))
+xlabel('Time(s)')
+ylabel('Motor Force(N)')
+% ylim([1.5 2.5])
+legend('motor 1','motor 2','motor 3','motor 4')
+
+disp('The settling time for the rates controller is:')
 disp(settlingTime)
+
+%% MPC Forward @ [2,0,1]
+clc
+
+load('Forward_MPC_posx.mat')
+load('Forward_MPC_posy.mat')
+load('Forward_MPC_posz.mat')
+load('Forward_MPC_time.mat')
+load('Forward_MPC_teltime.mat')
+load('Forward_MPC_mot1.mat')
+load('Forward_MPC_mot2.mat')
+load('Forward_MPC_mot3.mat')
+load('Forward_MPC_mot4.mat')
+
+Tindex = 1;
+
+for i = 1:length(posz)
+    if posz(i) > 0
+        Tindex = i-1;
+        break;
+    end
+end
+
+time = time - time(Tindex);
+
+for i = 1:length(time)
+    if time(i) > 6
+        Tindex = i;
+        break;
+    end
+end
+
+desPos = [2;0;0];
+lower = desPos - 0.02*desPos;
+upper = desPos + 0.02*desPos;
+ss_index = 1;
+
+for i = Tindex:length(posx)
+    if posx(i) < lower(1) || posx(i) > upper(1)
+        ss_index = i;
+    end
+end
+
+settlingTime = time(ss_index);
+forward_mpc_settlingTime = settlingTime;
+
+telTindex = floor(Tindex/2);
+teltime = teltime - teltime(telTindex);
+
+
+[M,I] = max(mot3);
+
+mpcinput = [mot1(I); mot2(I); mot3(I); mot4(I)];
+
+figure('Name','Forward MPC')
+subplot(2,1,1)
+plot(time(Tindex:end),posx(Tindex:end),'b')
+hold on
+plot(time(Tindex:end),posy(Tindex:end),'g')
+plot(time(Tindex:end),posz(Tindex:end),'r')
+xline(settlingTime,'k');
+yline(lower(1),'--k');
+yline(upper(1),'--k');
+ylim([0 2.1])
+xlabel('Time(s)')
+ylabel('Position(m)')
+legend('posx','posy','posz','settling time')
+title('MPC: Target [2,0,1]')
+
+subplot(2,1,2)
+plot(teltime(telTindex:end),mot1(telTindex:end))
+hold on
+plot(teltime(telTindex:end),mot2(telTindex:end))
+plot(teltime(telTindex:end),mot3(telTindex:end))
+plot(teltime(telTindex:end),mot4(telTindex:end))
+xlabel('Time(s)')
+ylabel('Motor Force(N)')
+% ylim([1.5 2.5])
+legend('motor 1','motor 2','motor 3','motor 4')
+
+disp('The settling time for the rates controller is:')
+disp(settlingTime)
+
+
 
